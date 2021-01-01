48d1415e5ab5e2f9e710a50deebb302579b4050c
eeryinkblot <mrcluedude@hotmail.de>
2021-01-01 13:08:33 +0100

    :fire: Removed some of the template code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Clock from "./components/Clock";
 import TextAreaCounter from "./components/TextAreaCounter";
@@ -7,20 +6,6 @@ import TextAreaCounter from "./components/TextAreaCounter";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Clock />
       <TextAreaCounter defaultText="Ultra" />
     </div>
