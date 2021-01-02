4b9be04651f9dc46fe29946a6165539eae9d71a3
eeryinkblot <mrcluedude@hotmail.de>
2021-01-02 16:13:20 +0100

    :white_check_mark: Changed test description

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-test('renders learn react link', () => {
+test('renders Hello world', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello, world!/i);
   expect(linkElement).toBeInTheDocument();
