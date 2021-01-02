2ed2ba0005c6ce807342941df3b2f8f22aa93e27
eeryinkblot <mrcluedude@hotmail.de>
2021-01-02 20:14:37 +0100

    :sparkles: Ascending sorting by click on the thead element

--- src/components/Excel.tsx
+++ src/components/Excel.tsx
@@ -1,24 +1,37 @@
 import React from "react";
 
-export default class Excel extends React.Component<any, any> {
-    displayName = "Excels";
+export default class Excel extends React.Component<any, { data: Array<Array<string>> }> {
+    constructor(props: any) {
+        super(props);
+        this.state = {data: props.initialData};
+    }
+
+    sort = (e: any) => {
+        const column = e.target.cellIndex;
+        const data = Array.from(this.state.data);
+        const sortedData = data.sort((a: Array<string>, b: Array<string>) => {
+            return a[column] < b[column] ? -1 : 1;
+        });
+        this.setState({data: sortedData});
+    }
+
     render() {
         let headers = this.props.headers;
-        let initialData = this.props.initialData;
+        let data = this.state.data;
         return (
             <table>
-                <thead>
+                <thead onClick={this.sort}>
                 <tr>
-                    {headers.map((title: string) =>
-                        <th>{title}</th>
+                    {headers.map((title: string, idx: number) =>
+                        <th key={idx}>{title}</th>
                     )}
                 </tr>
                 </thead>
                 <tbody>
-                {initialData.map((data: Array<string>) =>
-                    <tr>
-                        {data.map((field: string) =>
-                            <td>{field}</td>
+                {data.map((row: Array<string>, index: number) =>
+                    <tr key={index}>
+                        {row.map((cell: string, index: number) =>
+                            <td key={index}>{cell}</td>
                         )}
                     </tr>
                 )}
